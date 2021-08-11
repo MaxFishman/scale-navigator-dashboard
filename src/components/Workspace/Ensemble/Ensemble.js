1c7ba35af79eee78984fa21b06ed0be3d0f52b49
nturczan-calarts <nturczan@calarts.edu>
2021-08-11 16:16:38 -0700

    add ensemble card

--- src/components/Workspace/Ensemble/Ensemble.js
+++ src/components/Workspace/Ensemble/Ensemble.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { Input } from 'antd';
-
+import { Card, Avatar, Input } from 'antd';
+import Icon from '@ant-design/icons';
 const { Search } = Input;
+const { Meta } = Card;
+
 
 function Ensemble(){
   return(
-    <Search placeholder="input new ensemble name" enterButton="Create Ensemble" size="large"/>
+
+    <div style={{ background: '#ECECEC', padding: '30px' }}>
+      <Search placeholder="input new ensemble name" enterButton="Create Ensemble" size="large"/>
+      <h1>join an ensemble</h1>
+      <Card
+    style={{ width: 300 }}
+  >
+    <Meta
+      avatar={<Avatar src="https://www.vippng.com/png/detail/509-5094205_hexagon-rounded-corners-png.png" />}
+      title="Berliner Philharmoniker"
+    />
+    <p>Host: OctorDocktopus</p>
+    <p>3 members</p>
+  </Card>
+    </div>
+
   );
 }
 
