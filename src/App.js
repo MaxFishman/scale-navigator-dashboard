9983387d28a94c1ffc21b4b3ad0465e0d2a7ae3d
Nathan Ho <nathan.waikin.ho@gmail.com>
2021-08-13 09:28:48 -0700

    Move p5Sketch to root of application

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Workspace from './components/Workspace/Workspace';
+import Pfivesketch from './components/Navigation/Navigator/Pfivesketch';
 import { Col, Row } from 'antd';
 
 function App() {
+  const p5Sketch = Pfivesketch();
+
   return (
     <div className="App">
       <div id="grid">
-          <Navigation />
-          <Workspace /> 
+          <Navigation p5Sketch={ p5Sketch } />
+          <Workspace />
       </div>
     </div>
   );
