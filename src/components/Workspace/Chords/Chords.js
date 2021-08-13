380a6427e9e85878e59bf68dbd59afd045a28ed4
Nathan Ho <nathan.waikin.ho@gmail.com>
2021-08-13 11:40:53 -0700

    Add list of matching chords

--- src/components/Workspace/Chords/Chords.js
+++ src/components/Workspace/Chords/Chords.js
@@ -1,5 +1,34 @@
 import './ChordStyle.css';
 import React from 'react';
+import * as scalesJson from './pressing_scale_dict.json';
+import * as chordsJson from './chords_no_supersets.json';
+
+const SCALES = scalesJson.default;
+const CHORDS = chordsJson.default;
+
+const edo = 12;
+
+const isSubset = (array1, array2) => {
+  for (let x of array1) {
+    if (array2.indexOf(x) === -1) {
+      return false;
+    };
+  }
+  return true;
+};
+
+const getChordsInScale = (scale) => {
+  const result = [];
+  const pitchClasses = SCALES[scale].pitch_classes;
+  for (let chordName of Object.keys(CHORDS)) {
+    const chord = CHORDS[chordName];
+    const chordPitchClasses = chord.prime_form_kinda;
+    if (isSubset(chordPitchClasses, pitchClasses)) {
+      result.push(chordName);
+    }
+  }
+  return result;
+};
 
 class Chords extends React.Component {
   constructor(props) {
@@ -11,20 +40,25 @@ class Chords extends React.Component {
     });
 
     this.state = {
-      scale: "c_diatonic"
+      scale: "c_diatonic",
+      chords: "",
     };
     this.update();
   }
 
   update() {
+    const scale = this.props.navigator.main_polygon ?
+      this.props.navigator.main_polygon.scale
+      : "none";
+    const chords = scale === "none" ?
+      ""
+      : getChordsInScale(scale).join(" ");
     this.setState({
-      scale: (
-        this.props.navigator.main_polygon ?
-          this.props.navigator.main_polygon.scale
-          : "none"
-      )
+      scale: scale,
+      chords: chords
     });
-    // I'm sorry, I am bad at React and could not figure out how to avoid this.
+    // FIXME: forceUpdate is a code smell
+    // I'm sorry, I am bad at React and could not figure out how to avoid this
     this.forceUpdate();
   }
 
@@ -32,6 +66,7 @@ class Chords extends React.Component {
     return(
       <div id="Chords">
         <p>Current scale: { this.state.scale }</p>
+        <p>Pitch classes: { this.state.chords }</p>
       </div>
     );
   }
