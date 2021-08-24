e6cae6a9f728872a0e40a8d99a6ecb2fa5ac0c54
rudytak <75335232+rudytak@users.noreply.github.com>
2021-08-24 13:41:54 +0200

    Navigator scaling and mobile init

--- src/components/Navigation/Navigator/Pfivesketch.js
+++ src/components/Navigation/Navigator/Pfivesketch.js
@@ -6,66 +6,65 @@ import Sketch from "react-p5";
 const fps = 30;
 
 function Pfivesketch({ nav }) {
-  const setup = (p5, canvasParentRef) => {
-    var p = document.getElementById("canv_container").getBoundingClientRect();
-    p5.createCanvas(p.width, p.height).parent(canvasParentRef);
-    p5.frameRate(fps);
+    const setup = (p5, canvasParentRef) => {
+        var p = document.getElementById("canv_container").getBoundingClientRect();
+        p5.createCanvas(p.width, Math.max(p.height, document.body.getBoundingClientRect().height / 2)).parent(canvasParentRef);
+        p5.frameRate(fps);
 
-    nav.init(p5);
+        nav.init(p5);
 
-    windowResized(p5);
-  };
+        windowResized(p5);
+    };
 
-  const draw = (p5) => {
-    p5.clear();
-    nav.draw(p5);
+    const draw = (p5) => {
+        p5.clear();
+        nav.draw(p5);
 
-    if (
-      document.getElementById("autopilot_checkbox").checked !==
-      nav.autopilot_data.active
-    ) {
-      nav.toggle_autopilot();
-    }
-  };
+        if (
+            document.getElementById("autopilot_checkbox").checked !==
+            nav.autopilot_data.active
+        ) {
+            nav.toggle_autopilot();
+        }
+    };
 
-  const mousePressed = (p5) => {
-    nav.mousePressed(p5);
-  };
+    const mousePressed = (p5) => {
+        nav.mousePressed(p5);
+    };
 
-  const mouseReleased = (p5) => {
-    nav.mouseReleased(p5);
-  };
+    const mouseReleased = (p5) => {
+        nav.mouseReleased(p5);
+    };
 
-  const windowResized = (p5) => {
-    var p = document.getElementById("canv_container").getBoundingClientRect();
-    p5.resizeCanvas(p.width, p.height);
-  };
+    const windowResized = (p5) => {
+        var p = document.getElementById("canv_container").getBoundingClientRect();
+        p5.resizeCanvas(p.width, Math.max(p.height, document.body.getBoundingClientRect().height / 2));
+    };
 
-  return (
-    <Sketch
-      setup={setup}
-      draw={draw}
-      mousePressed={mousePressed}
-      mouseReleased={mouseReleased}
-      windowResized={windowResized}
-      navigator={nav}
-    />
-  );
+    return ( <
+        Sketch setup = { setup }
+        draw = { draw }
+        mousePressed = { mousePressed }
+        mouseReleased = { mouseReleased }
+        windowResized = { windowResized }
+        navigator = { nav }
+        />
+    );
 }
 
 Pfivesketch.note_names = [
-  "C",
-  "D♭",
-  "D",
-  "E♭",
-  "E",
-  "F",
-  "F#",
-  "G",
-  "A♭",
-  "A",
-  "B♭",
-  "B",
+    "C",
+    "D♭",
+    "D",
+    "E♭",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "A♭",
+    "A",
+    "B♭",
+    "B",
 ];
 Pfivesketch.fps = fps;
 
