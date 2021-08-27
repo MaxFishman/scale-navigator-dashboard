a67e53c1fb0bb35ca4163c1a96732b54d807feea
Michael Maurer <mjmaurer777@gmail.com>
2021-08-26 23:10:26 -0400

    Fix chord bug by moving state to top-level component. Chords tab just toggles playing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import "./App.scss";
 import Navigation from "./components/Navigation/Navigation";
 import Workspace from "./components/Workspace/Workspace";
 import { ScaleContext } from "./components/Context/ScaleContext";
+import { ChordContext } from "./components/Context/ChordContext";
 import Navigator from "./components/Navigation/Navigator/Navigator";
 import React, { useState, useRef } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import Chords from "components/ToneJS/Chord";
 
 function App() {
   const [scaleData, setScaleData] = useState({
     scale: "c_diatonic",
+  });
+  const [chordData, setChordData] = useState({
+    playing: false,
   });
   // TODO(mjmaurer): I think ideally workspace shouldn't have to know
   // about the navigator, but right now it's surface is pretty large.
@@ -18,16 +23,28 @@ function App() {
   return (
     <div className="appcontainer">
       <div className="contentcontainer">
+        <Chords
+          navigator={navRef.current}
+          scaleData={scaleData}
+          chordData={chordData}
+        ></Chords>
         <Router>
-          <ScaleContext.Provider
+          <ChordContext.Provider
             value={{
-              ...scaleData,
-              navigator: navRef.current,
+              chordData,
+              setChordData,
             }}
           >
-            <Navigation />
-            <Workspace />
-          </ScaleContext.Provider>
+            <ScaleContext.Provider
+              value={{
+                ...scaleData,
+                navigator: navRef.current,
+              }}
+            >
+              <Navigation />
+              <Workspace />
+            </ScaleContext.Provider>
+          </ChordContext.Provider>
         </Router>
       </div>
     </div>
