71d6ad54fbf4ca0f1e8f9109918e55899203e4c1
nturczan-calarts <nturczan@calarts.edu>
2021-08-30 09:15:48 -0700

    fix merge

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,75 +1,85 @@
 import Pfivesketch from "./Navigator/Pfivesketch";
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { ScaleContext } from "../Context/ScaleContext";
 import ScaleData from "common/ScaleData";
 import PitchClassData from "common/PitchClassData";
 import "./Navigation.scss";
+import Navigator from "./Navigator/Navigator";
 
 const Navigation = () => {
+  const { scaleData, setScaleData } = useContext(ScaleContext);
+  const { scale } = scaleData;
+
+  const navRef = useRef(null);
+
+  useEffect(() => {
+    navRef.current = new Navigator.Navigator(setScaleData);
+  }, []);
+
+  useEffect(() => {
+    navRef.current.jumpToScale(scale);
+  }, [scale]);
+
+  useEffect(() => {
+    navRef.current.scaleDataCallback(setScaleData);
+  }, [setScaleData]);
+
   return (
-    <ScaleContext.Consumer>
-      {({ navigator, scale }) => {
-        return (
-          <div className="navigation">
-            <div className="navigation__logowrap">
-              <h1 className="navigation__logo">Scale Navigator</h1>
-              <h2 className="navigation__sublogo">DASHBOARD</h2>
-            </div>
-            <div className="navigation__scalenav" id="canv_container">
-              <Pfivesketch nav={navigator}></Pfivesketch>
-            </div>
-            <div className="navinfo">
-              <div className="navinfo__note">
-                <h5>NOTE</h5>
-                {PitchClassData[ScaleData[scale].root].note}
-              </div>
-              <div className="navinfo__scale">
-                <h5>SCALE</h5>
-                {ScaleData[scale].scale_class
-                  .split("_")
-                  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-                  .join(" ")}
-              </div>
-              <div className="navinfo__options">
-                <div className="navinfo__option">
-                  <input
-                    type="checkbox"
-                    autoComplete="off"
-                    name="autopilot"
-                    id="autopilot_checkbox"
-                  ></input>
-                  <label for="autopilot">Autopilot</label>
-                </div>
-               
-                <div className="navinfo__option">
-                  <input
-                    type="range"
-                    autoComplete="off"
-                    name="autopilot_interval"
-                    id="autopilot_interval"
-                    min="1"
-                    max="4"
-                    step="0.01"
-                  ></input>
-                  <label for="labels">Tempo</label>
-                </div>
-              </div>
-              <div className="navinfo__option">
-                  <input
-                    type="checkbox"
-                    autoComplete="off"
-                    name="labels"
-                    id="labels_checkbox"
-                    defaultChecked="false"
-                    style={{visibility:'hidden'}}
-                  ></input>
-                  {/* <label for="labels">Labels</label> */}
-                </div>
-            </div>
+    <div className="navigation">
+      <div className="navigation__logowrap">
+        <h1 className="navigation__logo">Scale Navigator</h1>
+        <h2 className="navigation__sublogo">DASHBOARD</h2>
+      </div>
+      <div className="navigation__scalenav" id="canv_container">
+        <Pfivesketch nav={navRef.current}></Pfivesketch>
+      </div>
+      <div className="navinfo">
+        <div className="navinfo__note">
+          <h5>NOTE</h5>
+          {PitchClassData[ScaleData[scale].root].note}
+        </div>
+        <div className="navinfo__scale">
+          <h5>SCALE</h5>
+          {ScaleData[scale].scale_class
+            .split("_")
+            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(" ")}
+        </div>
+        <div className="navinfo__options">
+          <div className="navinfo__option">
+            <input
+              type="checkbox"
+              autoComplete="off"
+              name="autopilot"
+              id="autopilot_checkbox"
+            ></input>
+            <label for="autopilot">Autopilot</label>
           </div>
-        );
-      }}
-    </ScaleContext.Consumer>
+          <div className="navinfo__option">
+            <input
+              type="checkbox"
+              autoComplete="off"
+              name="labels"
+              id="labels_checkbox"
+              defaultChecked="false"
+            ></input>
+            <label for="labels">Labels</label>
+          </div>
+          <div className="navinfo__option">
+            <input
+              type="range"
+              autoComplete="off"
+              name="autopilot_interval"
+              id="autopilot_interval"
+              min="1"
+              max="4"
+              step="0.01"
+            ></input>
+            <label for="labels">Interval</label>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
