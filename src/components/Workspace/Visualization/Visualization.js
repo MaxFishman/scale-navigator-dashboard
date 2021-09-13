bede487a9fc984e2cc000946dcf679d75af58888
nturczan-calarts <nturczan@calarts.edu>
2021-09-13 11:03:23 -0700

    add force graph library and vis template

---
+++ src/components/Workspace/Visualization/Visualization.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  ForceGraph2D,
+  ForceGraph3D,
+  ForceGraphVR,
+  ForceGraphAR,
+} from "react-force-graph";
+
+const Visualization = () => {
+  return (
+    <p> hey waht</p>
+    <ForceGraph3D graphData={myData} />;
+    )
+};
+
+export default Visualization;
