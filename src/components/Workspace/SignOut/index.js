6d585ea28f76ad8630e6e4b895746b27a78c5a55
Scott Devereux <scott.devereux@gmail.com>
2021-09-28 11:23:26 +0100

    Display account details for auth user and sign up page for unAuth

--- src/components/Workspace/SignOut/index.js
+++ src/components/Workspace/SignOut/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { withFirebase } from '../../Firebase';
 
 const SignOutButton = ({ firebase }) => (
-  <button type="button" onClick={firebase.doSignOut}>
+  <button type="button" style={{backgroundColor:'white', color:'red'}} onClick={firebase.doSignOut}>
     Sign Out
   </button>
 );
