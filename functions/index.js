6ffaf1f4ee5d77231990aa09438b4a61a15126b4
Scott Devereux <scott.devereux@gmail.com>
2021-10-14 16:35:21 +0100

    Firebase function for ecomm server

---
+++ functions/index.js
@@ -0,0 +1,33 @@
+const functions = require('firebase-functions');
+
+const express = require("express");
+const app = express();
+// This is your real test secret API key.
+const stripe = require("stripe")("sk_test_ju1E3Y8rI8Xj9JFM2GaKP9KO00wVoTsAJX");
+
+
+
+app.use(express.static("public"));
+app.use(express.json());
+
+const calculateOrderAmount = items => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400;
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 29.99,
+    currency: "usd"
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  });
+});
+
+app.listen(4242, () => console.log('Node server listening on port 4242!'));
