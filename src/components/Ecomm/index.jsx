e0b93f3d0b487a139244945d20dd37980e328bac
Scott Devereux <scott.devereux@gmail.com>
2021-10-14 16:34:26 +0100

    Fixed css override

--- src/components/Ecomm/index.jsx
+++ src/components/Ecomm/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
-import "./Ecomm.css";
+
 
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
