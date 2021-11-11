ddd382f7714e2428401aa64edcbe10d73e6c232b
Michael Maurer <mjmaurer777@gmail.com>
2021-11-10 19:06:03 -0500

    Remove ref useEffect

--- src/components/ToneJS/Chord.js
+++ src/components/ToneJS/Chord.js
@@ -1,31 +1,37 @@
-import { useSelector, useDispatch } from 'react-redux'
-import React, { useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import React, { useCallback, useEffect, useRef } from "react";
 import * as Tone from "tone";
 import ChordChooser from "./ChordChooser";
 
 const midiToHz = (midiNote) => {
-  return 440 * Math.pow(2, (midiNote - 69) / 12);
+    return 440 * Math.pow(2, (midiNote - 69) / 12);
 };
 
 function Chords() {
-    const dispatch = useDispatch()
-    const { chordData, scaleData } = useSelector(state => state.root)
+    const dispatch = useDispatch();
+    const { chordData, scaleData } = useSelector((state) => state.root);
 
-    const { scale, scaleIndex } = scaleData;
+    const { scale } = scaleData;
 
-    const _setChordData = (payload) => dispatch({ type: 'SET_CHORD_DATA', payload })
+    const _setChordData = useCallback(
+        (payload) => dispatch({ type: "SET_CHORD_DATA", payload }),
+        [dispatch]
+    );
 
     const chordChooserRef = useRef(null);
     const synthRef = useRef(null);
 
-    useEffect(() => {
+    if (chordChooserRef.current == null) {
         chordChooserRef.current = new ChordChooser(
             chordData,
             _setChordData,
             scale
         );
+    }
+
+    if (synthRef.current == null) {
         synthRef.current = new Tone.PolySynth({ volume: -20 }).toDestination();
-    }, []);
+    }
 
     useEffect(() => {
         chordChooserRef.current.setChordDataContext(chordData, _setChordData);
@@ -33,7 +39,7 @@ function Chords() {
 
     useEffect(() => {
         chordChooserRef.current.tick(scale);
-    }, [scale, scaleIndex]);
+    }, [scale]);
 
     useEffect(() => {
         const { playing, chord, previousChord } = chordData;
@@ -43,7 +49,8 @@ function Chords() {
             previousChord.root !== chord.root ||
             previousChord.chord_type !== chord.chord_type;
 
-        const notes = chord === null ? [] : chord.original_voicing.map(midiToHz);
+        const notes =
+            chord === null ? [] : chord.original_voicing.map(midiToHz);
 
         if (playing && isNewChord) {
             synthRef.current.triggerAttack(notes);
