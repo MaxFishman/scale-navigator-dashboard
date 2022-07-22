ac606958a5fb333d19fef0ae8704db529b48c065
nathanturczan <nathanturczan@gmail.com>
2022-07-22 09:49:54 -0700

    add autoharp

--- src/components/Workspace/Tablature/instruments/autoharp/Autoharp.js
+++ src/components/Workspace/Tablature/instruments/autoharp/Autoharp.js
@@ -3,71 +3,137 @@ import "./Autoharp.scss";
 import classNames from "classnames";
 
 export default function Autoharp({ keyData }) {
-  return (
-    <>
-      <div class="autoharp">
-        <div
-          class={classNames("piano__button piano__button--white c", {
-            "piano__button--on": keyData.pitch_classes.includes(0),
-          })}
-        ></div>
-        <div
-          class={classNames("piano__button piano__button--black c_sharp", {
-            "piano__button--on": keyData.pitch_classes.includes(1),
-          })}
-        ></div>
-        <div
-          class={classNames("piano__button piano__button--white d", {
-            "piano__button--on": keyData.pitch_classes.includes(2),
-          })}
-        ></div>
-        <div
-          class={classNames("piano__button piano__button--black d_sharp", {
-            "piano__button--on": keyData.pitch_classes.includes(3),
-          })}
-        ></div>
-        <div
-          class={classNames("piano__button piano__button--white e", {
-            "piano__button--on": keyData.pitch_classes.includes(4),
-          })}
-        ></div>
-        <div
-          class={classNames("piano__button piano__button--white f", {
-            "piano__button--on": keyData.pitch_classes.includes(5),
-          })}
-        ></div>
-        <div
-          class={classNames("piano__button piano__button--black f_sharp", {
-            "piano__button--on": keyData.pitch_classes.includes(6),
-          })}
-        ></div>
-        <div
-          class={classNames("piano__button piano__button--white g", {
-            "piano__button--on": keyData.pitch_classes.includes(7),
-          })}
-        ></div>
-        <div
-          class={classNames("piano__button piano__button--black g_sharp", {
-            "piano__button--on": keyData.pitch_classes.includes(8),
-          })}
-        ></div>
-        <div
-          class={classNames("piano__button piano__button--white a", {
-            "piano__button--on": keyData.pitch_classes.includes(9),
-          })}
-        ></div>
-        <div
-          class={classNames("piano__button piano__button--black a_sharp", {
-            "piano__button--on": keyData.pitch_classes.includes(10),
-          })}
-        ></div>
-        <div
-          class={classNames("piano__button piano__button--white b", {
-            "piano__button--on": keyData.pitch_classes.includes(11),
-          })}
-        ></div>
-      </div>
-      <div class="piano__clear"></div>
-    </>
-  );
+    return (
+        <>
+            <div class="autoharp">
+                <div
+                    class={classNames("autoharp__button autoharp__button c", {
+                        "autoharp__button--on":
+                            keyData.pitch_classes.includes(0),
+                    })}
+                >
+                    <h2>C</h2>
+                </div>
+                <div
+                    class={classNames(
+                        "autoharp__button autoharp__button d_sharp",
+                        {
+                            "autoharp__button--on":
+                                keyData.pitch_classes.includes(3),
+                        }
+                    )}
+                >
+                    <h2>
+                        E <em>b</em>
+                    </h2>
+                </div>
+                <div
+                    class={classNames(
+                        "autoharp__button autoharp__button f_sharp",
+                        {
+                            "autoharp__button--on":
+                                keyData.pitch_classes.includes(6),
+                        }
+                    )}
+                >
+                    <h2>
+                        F<em>#</em>
+                    </h2>
+                </div>
+                <div
+                    class={classNames("autoharp__button autoharp__button a", {
+                        "autoharp__button--on":
+                            keyData.pitch_classes.includes(9),
+                    })}
+                >
+                    <h2>A</h2>
+                </div>
+                <div
+                    class={classNames(
+                        "autoharp__button autoharp__button c_sharp",
+                        {
+                            "autoharp__button--on":
+                                keyData.pitch_classes.includes(1),
+                        }
+                    )}
+                >
+                    <h2>
+                        C<em>#</em>
+                    </h2>
+                </div>
+                <div
+                    class={classNames("autoharp__button autoharp__button e", {
+                        "autoharp__button--on":
+                            keyData.pitch_classes.includes(4),
+                    })}
+                >
+                    {" "}
+                    <h2>E</h2>
+                </div>
+                <div
+                    class={classNames("autoharp__button autoharp__button g", {
+                        "autoharp__button--on":
+                            keyData.pitch_classes.includes(7),
+                    })}
+                >
+                    <h2>G</h2>
+                </div>
+                <div
+                    class={classNames(
+                        "autoharp__button autoharp__button a_sharp",
+                        {
+                            "autoharp__button--on":
+                                keyData.pitch_classes.includes(10),
+                        }
+                    )}
+                >
+                    <h2>
+                        B <em>b</em>
+                    </h2>
+                </div>
+                <div
+                    class={classNames("autoharp__button autoharp__button d", {
+                        "autoharp__button--on":
+                            keyData.pitch_classes.includes(2),
+                    })}
+                >
+                    <h2>D</h2>
+                </div>
+
+                <div
+                    class={classNames("autoharp__button autoharp__button f", {
+                        "autoharp__button--on":
+                            keyData.pitch_classes.includes(5),
+                    })}
+                >
+                    {" "}
+                    <h2>F</h2>
+                </div>
+
+                <div
+                    class={classNames(
+                        "autoharp__button autoharp__button g_sharp",
+                        {
+                            "autoharp__button--on":
+                                keyData.pitch_classes.includes(8),
+                        }
+                    )}
+                >
+                    <h2>
+                        G<em>#</em>
+                    </h2>
+                </div>
+
+                <div
+                    class={classNames("autoharp__button autoharp__button b", {
+                        "autoharp__button--on":
+                            keyData.pitch_classes.includes(11),
+                    })}
+                >
+                    <h2>B</h2>
+                </div>
+            </div>
+            <div class="autoharp__clear"></div>
+        </>
+    );
 }
